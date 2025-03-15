c64951697b5b67072202a29d34b569a676f496a9
rajaram <vrajarampandian@gmail.com>
2025-03-16 05:20:21 +0530

    Updated comments

--- IO_Manipulator/Boolean.cpp
+++ IO_Manipulator/Boolean.cpp
@@ -3,13 +3,28 @@ using namespace std;
 
 int main()
 {
+    // Declare a boolean variable 'i' and initialize it to true
     bool i = true;
+    // Enable boolalpha to print boolean values as true/false instead of 1/0
     cout << std::boolalpha;
+
+    // Print the value of 'i' (true) as "true"
     cout << i << "\n";
+
+    // Declare a boolean variable 'j' and initialize it to false
     bool j = false;
+
+    // Print the value of 'j' (false) as "false"
     cout << j << "\n";
+
+    // Declare a boolean variable 'z' and initialize it to true
     bool z = true;
+
+    // Disable boolalpha to print boolean values as 1/0 instead of true/false
     cout << std::noboolalpha;
+
+    // Print the value of 'z' (true) as "1"
     cout << z;
+
     return 0;
 }
